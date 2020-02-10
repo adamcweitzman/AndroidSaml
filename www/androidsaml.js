739d11900dcbb00db10cc8f9512cb387c9678fd0
Adam Weitzman <aweitzman@devonway.com>
2020-02-10 11:29:10 -0800

    add in boilerplate for plugin

---
+++ www/androidsaml.js
@@ -0,0 +1,22 @@
+// Empty constructor
+function AndroidSaml() {}
+
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+AndroidSaml.prototype.show = function(message, duration, successCallback, errorCallback) {
+  var options = {};
+  options.message = message;
+  options.duration = duration;
+  cordova.exec(successCallback, errorCallback, 'AndroidSaml', 'show', [options]);
+}
+
+// Installation constructor that binds ToastyPlugin to window
+AndroidSaml.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.androidSaml = new AndroidSaml();
+  return window.plugins.anroidSaml;
+};
+
+cordova.addConstructor(AndroidSaml.install);
