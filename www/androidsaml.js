e8c28336c9acce46e637d33a26635c0169682a08
Adam Weitzman <aweitzman@devonway.com>
2020-02-11 15:22:19 -0800

    module changes

--- www/androidsaml.js
+++ www/androidsaml.js
@@ -3,11 +3,13 @@ function AndroidSaml() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-AndroidSaml.prototype.echo = function(message, successCallback, errorCallback) {
+AndroidSaml.echo = function(message, successCallback, errorCallback) {
   var options = {};
   options.message = message;
-  options.duration = duration;
+  
   cordova.exec(successCallback, errorCallback, 'AndroidSaml', 'echo', [options]);
+
+  return 'RETURNED FROM PLUGIN';
 }
 
 
@@ -20,4 +22,4 @@ AndroidSaml.install = function() {
   return window.plugins.anroidSaml;
 };
 
-cordova.addConstructor(AndroidSaml.install);
+module.exports = AndroidSaml;
