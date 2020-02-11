fba0e51dcb7615736a417c6098ea20a306e9c31f
Adam Weitzman <aweitzman@devonway.com>
2020-02-11 15:46:58 -0800

    change namespacing

--- src/android/AndroidSaml.java
+++ src/android/AndroidSaml.java
@@ -1,4 +1,4 @@
-package com.adamweitzman.cordova.plugin.AndroidSaml;
+package com.adamweitzman.cordova.plugin;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
