707a5c6614c6b6b921ab38d84c06ebae048524e0
Adam Weitzman <aweitzman@devonway.com>
2020-02-12 11:54:01 -0800

    add web view

--- src/android/AndroidSaml.java
+++ src/android/AndroidSaml.java
@@ -7,6 +7,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.webkit.WebView;
+
 /**
 * This class echoes a string called from JavaScript.
 */
@@ -23,11 +25,24 @@ public class AndroidSaml extends CordovaPlugin {
     }
 
     private void echo(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message + "coming from android");
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+
+        WebView myWebView = new WebView(activityContext);
+        setContentView(myWebView);
+
+        myWebView.loadUrl("https://www.google.com");
+
+        callbackContext.success(message + "coming from android");
+        // if (message != null && message.length() > 0) {
+        //     callbackContext.success(message + "coming from android");
+        // } else {
+        //     callbackContext.error("Expected one non-empty string argument.");
+        // }
+
+        //create new web view
+        //create listener to http requests (redirects specifically) and return null to not redirect
+        //open to pased in url
+        //when url loads, pass the data found in the form back to the ui
+        //ui call the success function
     }
 }
 
