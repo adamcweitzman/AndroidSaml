616573e8f30c5086baebc0050ee5de8c2b307b80
Adam Weitzman <aweitzman@devonway.com>
2020-02-13 11:32:57 -0800

    test

--- src/android/AndroidSaml.java
+++ src/android/AndroidSaml.java
@@ -8,11 +8,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
 * This class echoes a string called from JavaScript.
 */
 public class AndroidSaml extends CordovaPlugin {
+
+    private WebView inAppWebView;
+    private InAppBrowserClient currentClient;
     
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -25,13 +29,26 @@ public class AndroidSaml extends CordovaPlugin {
     }
 
     private void echo(String message, CallbackContext callbackContext) {
+            this.cordova.getActivity().runOnUiThread(new Runnable() {
 
-        WebView myWebView = new WebView(activityContext);
-        setContentView(myWebView);
+                @SuppressLint("NewApi")
+                @Override
+                public void run() {
+                    currentClient.waitForBeforeload = false;
+                    inAppWebView.setWebViewClient(currentClient);
 
-        myWebView.loadUrl("https://www.google.com");
+                    // ((InAppBrowserClient)inAppWebView.getWebViewClient()).waitForBeforeload = false;
+                    
+                    // inAppWebView.loadUrl(url);
+                }
+            });
 
-        callbackContext.success(message + "coming from android");
+        // WebView myWebView = new WebView(activityContext);
+        // setContentView(myWebView);
+
+        // myWebView.loadUrl("https://www.google.com");
+
+        // callbackContext.success(message + "coming from android");
         // if (message != null && message.length() > 0) {
         //     callbackContext.success(message + "coming from android");
         // } else {
