9921d3602946dfe718468556d7e0b3c192315305
Adam Weitzman <aweitzman@devonway.com>
2020-03-05 15:13:41 -0800

    getting bearer token back

--- src/android/AndroidSamlBrowserClient.java
+++ src/android/AndroidSamlBrowserClient.java
@@ -1,10 +1,12 @@
 package com.adamweitzman.cordova.plugin;
 
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import org.apache.cordova.CordovaWebView;
 
 public class AndroidSamlBrowserClient extends WebViewClient {
-//    EditText edittext;
+    //    EditText edittext;
     CordovaWebView webView;
     String beforeload;
     boolean waitForBeforeload;
@@ -14,5 +16,17 @@ public class AndroidSamlBrowserClient extends WebViewClient {
 //        this.edittext = mEditText;
 //        this.beforeload = beforeload;
 //        this.waitForBeforeload = beforeload != null;
+    }
+
+    // TODO: 3/5/20 i think i have the bearer token now, figure out how to get that back from the app
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        view.loadUrl("javascript:" + "(function(){var el = document.getElementsByName(\"SAMLResponse\")[0]; return (el ? el.value : null);})()" );
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest request) {
+        return true;
     }
 }
